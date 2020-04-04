f95942b0286d4df6681f6990f0cc349452b00260
mikeku1116 <mikekuforgit@gmail.com>
2020-04-04 19:00:41 +0800

    完成照片上傳功能

--- photos/views.py
+++ photos/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UploadModelForm
 
 
 def index(request):
 
     form = UploadModelForm()
+
+    if request.method == "POST":
+        form = UploadModelForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/photos')
 
     context = {
         'form': form
