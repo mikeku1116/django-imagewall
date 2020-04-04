a5a6de075a4052c17aa869231cd7c07f18241edf
mikeku1116 <mikekuforgit@gmail.com>
2020-04-04 21:33:35 +0800

    修改Administration的Photo資料模型欄位顯示

--- photos/admin.py
+++ photos/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Photo
 
 
-admin.site.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('image', 'upload_date')
+
+
+admin.site.register(Photo, PhotoAdmin)
