244d68a5bfc2daf2eb6bb4e4631ab81b5425ecfd
mikeku1116 <mikekuforgit@gmail.com>
2020-04-04 16:47:40 +0800

    建立Photo資料模型及執行migration

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to='image/', blank=True, null=True)
+    upload_date = models.DateField(default=timezone.now)
+
